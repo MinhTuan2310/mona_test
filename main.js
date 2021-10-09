c8737e03997d76129f3515e6f67d37007e32f2a3
Tuan Le <lmtuan2310@gmail.com>
2021-10-09 08:55:08 +0700

    update toggle menu event

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const toggleMenu = document.querySelector('.toggle__menu');
 const navMenu = document.querySelector('.nav__menu');
+const liElementList = document.querySelectorAll('.nav__menu li');
 
+// toggle__menu's event click
 function handleShowNavMenu() {
   navMenu.classList.toggle('show');
   toggleMenu.classList.toggle('fa-times');
   toggleMenu.classList.toggle('fa-bars');
-
   toggleMenu.classList.contains('fa-times') ? toggleMenu.style.color = 'white' : toggleMenu.style.color = 'black';
 }
 
 toggleMenu.addEventListener('click', handleShowNavMenu);
+
+// remove class show when click on li;
+liElementList.forEach(liElement => {
+  liElement.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+    toggleMenu.classList.remove('fa-times','fa-bars');
+    toggleMenu.classList.add('fa-bars');
+    toggleMenu.classList.contains('fa-times') ? toggleMenu.style.color = 'white' : toggleMenu.style.color = 'black';
+  })
+});
+
+
